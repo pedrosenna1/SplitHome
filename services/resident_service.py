67b741fc9125fa1eae6ef8cfa9303fd8f076cae9
unknown <pedrotjb7@hotmail.com>
2026-01-27 11:24:49 -0300

    criacao da rota de deletar resident, ajuste no frontend com criacao dos botoes de ver/deletar morador

--- services/resident_service.py
+++ services/resident_service.py
@@ -1,16 +1,19 @@
 import sqlite3
 import os
 from dotenv import load_dotenv
+from utils.email_validador import is_valid_email
 
 load_dotenv()
 
 class ResidentService:
     @staticmethod
-    def create_resident(name,sobrenome,session_id):
+    def create_resident(name: str,sobrenome: str, email: str, telefone: str, session_id: int) -> bool:
+        if not is_valid_email(email):
+            return False
         try:
             conn = sqlite3.connect(os.getenv("DATABASE_PATH"))
             c = conn.cursor()
-            c.execute("INSERT INTO resident (name, sobrenome, session_id) VALUES (?, ?, ?)", (name, sobrenome, session_id))
+            c.execute("INSERT INTO resident (nome, sobrenome, email, telefone, user_id) VALUES (?, ?, ?, ?, ?)", (name, sobrenome, email, telefone, session_id))
             conn.commit()
             conn.close()
             return True
@@ -18,16 +21,30 @@ class ResidentService:
             return False
         
     @staticmethod
-    def get_residents_by_session(session_id):
+    def get_residents_by_session(session_id: int):
         try:
             conn = sqlite3.connect(os.getenv("DATABASE_PATH"))
             conn.row_factory = sqlite3.Row
             c = conn.cursor()
-            c.execute("SELECT id, name, sobrenome FROM resident WHERE session_id = ?", (session_id,))
+            
+            c.execute("SELECT id, nome, sobrenome, email, telefone FROM resident WHERE user_id = ?", (session_id,))
             residents = c.fetchall()
             conn.close()
+            
             return residents
         except sqlite3.Error:
             return []
         
+    @staticmethod
+    def deletar_resident(resident_id: int) -> bool:
+        try:
+            conn = sqlite3.connect(os.getenv("DATABASE_PATH"))
+            c = conn.cursor()
+            c.execute("DELETE FROM resident WHERE id = ?", (resident_id,))
+            conn.commit()
+            conn.close()
+            return True
+        except sqlite3.Error:
+            return False
+
         
---
+++ utils/email_validador.py
@@ -0,0 +1,9 @@
+from email_validator import validate_email, EmailNotValidError
+
+def is_valid_email(email):
+    try:
+        validate_email(email)
+        return True
+    except EmailNotValidError as e:
+        print(str(e))
+        return False
